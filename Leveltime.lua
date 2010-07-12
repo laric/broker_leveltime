3478587f7e1d593cc294bb3e5d57c43fb8b2cb4e
laric <laric@dummy.example.com>
2010-07-12 18:30:43 +0000

    Fixed the loadondemand flag. Started to add real code in the addon.
    Found out that using the event for TIME_PLAYED_MSG actully prints it out in the client. Not exactly what I hoped but what the heck

--- Leveltime.lua
+++ Leveltime.lua
@@ -1,4 +1,8 @@
 local addon = LibStub("AceAddon-3.0"):NewAddon("Broker_Leveltime","AceTimer-3.0","AceEvent-3.0")
+addon.totaltime = 0
+addon.playedthislevel = 0
+addon.basetime = 0
+addon.difftime = 0
 local f = CreateFrame("frame")
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 local dataobj = ldb:NewDataObject("Broker_Leveltime", {
@@ -9,6 +13,30 @@ local dataobj = ldb:NewDataObject("Broker_Leveltime", {
 
 function dataobj:OnTooltipShow()
 	self:AddLine("Starting point")
+	local tottime = addon.totaltime + addon.difftime
+	local lvltime = addon.playedthislevel + addon.difftime
+	local totdays = floor( tottime / (60*60*24))
+	local tothours = floor( (tottime-(totdays*60*60*24)) / (60*60))
+	local totmins = floor( (tottime-(totdays*60*60*24)-(tothours*60*60)) / 60 )
+	if totdays > 0  then
+	  self:AddLine("Total time played: " .. totdays .. " day(s) " .. tothours .. " hour(s) " .. totmins .. " min(s)")
+	elseif tothours > 0 then 
+	  self:AddLine("Total time played: " .. tothours .. " hour(s) " .. totmins .. " min(s)")
+	else
+	  self:AddLine("Total time played: " .. totmins .. " min(s)")
+    end	
+	local lvldays = floor( lvltime / (60*60*24))
+	local lvlhours = floor( (lvltime-(lvldays*60*60*24)) / (60*60))
+	local lvlmins = floor( (lvltime-(lvldays*60*60*24)-(lvlhours*60*60)) / 60 )
+	if lvldays > 0  then
+	  self:AddLine("Time played this level: " .. lvldays .. " day(s) " .. lvlhours .. " hour(s) " .. lvlmins .. " min(s)")
+	elseif lvlhours > 0 then 
+	  self:AddLine("Time played this level: " .. lvlhours .. " hour(s) " .. lvlmins .. " min(s)")
+	else
+	  self:AddLine("Time played this level: " .. lvlmins .. " min(s)")
+    end	
+	
+	
 end
 
 function dataobj:OnEnter()
@@ -23,6 +51,19 @@ function dataobj:OnLeave()
 	GameTooltip:Hide()
 end
 
+function addon:TIME_PLAYED_MSG(eventName, total, level)
+  self.totaltime = total
+  self.playedthislevel = level
+  self.basetime = time()
+end
+
+function addon:MinTimer()
+  self.difftime = time() - self.basetime
+end
+
 function addon:OnInitialize()
   self.db = LibStub("AceDB-3.0"):New("LeveltimeDB")
+  self:RegisterEvent("TIME_PLAYED_MSG")
+  self.minuteTimer = self:ScheduleRepeatingTimer("MinTimer", 60)
+  RequestTimePlayed()
 end
