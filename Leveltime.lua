a2fdb695392d455cea3ae5014359ad1c3f43b0f9
laric <laric@dummy.example.com>
2010-07-13 17:17:49 +0000

    Should work with basic functionality in now.

--- Leveltime.lua
+++ Leveltime.lua
@@ -33,7 +33,37 @@ function dataobj:OnTooltipShow()
 	  self:AddLine("Time played this level: " .. lvlhours .. " hour(s) " .. lvlmins .. " min(s)")
 	else
 	  self:AddLine("Time played this level: " .. lvlmins .. " min(s)")
-    end	
+    end
+	if addon.db.char.level[UnitLevel("player")] then
+		if addon.db.char.level[UnitLevel("player")].time then 
+		  self:AddLine("Current level was achieved ".. date("%c",addon.db.char.level[UnitLevel("player")].time))
+		end
+	end
+	
+	local printlevel=UnitLevel("player") - 1
+	if printlevel < 0 then printlevel = 1 end
+	local printtolevel = UnitLevel("player") - 5
+	if printtolevel < 0 then printtolevel = 1 end
+	while printlevel >= printtolevel do
+	  if addon.db.char.level[printlevel] then
+	    if addon.db.char.level[printlevel].time then 
+		  self:AddLine("Level "..printlevel.." was achieved ".. date("%c",addon.db.char.level[printlevel].time))
+		end
+	    if addon.db.char.level[printlevel].playtime then
+		  local tmptime = addon.db.char.level[printlevel].playtime
+		  local ptdays = floor(tmptime / (60*60*24))
+		  local pthours = floor((tmptime - (ptdays * 60*60*24)) / (60*60))
+		  local ptmins = floor((tmptime - (ptdays * 60*60*24) - (pthours *60*60)) / 60)
+		  local ptstr = ""
+		  if ptdays>0 then ptstr = ptdays.." day(s) " end
+		  if pthours>0 then ptstr = ptstr..pthours.." hour(s) " end
+		  ptstr = ptstr..ptmins.." min(s)" 
+		  self:AddLine("Level "..printlevel.." playtime was "..ptstr)
+		end
+	  end
+	  
+	  printlevel = printlevel-1
+	end
 	
 	
 end
@@ -57,9 +87,12 @@ function addon:TIME_PLAYED_MSG(eventName, total, level)
 end
 
 function addon:PLAYER_LEVEL_UP(eventName,level, hp, mp, talentPoints, strength, agility, stamina, intellect, spirit)
-  RequestTimePlayed()
   if not self.db.char.level then self.db.char.level={} end
+  if not self.db.char.level[level] then self.db.char.level[level]={} end
+  if not self.db.char.level[level-1] then self.db.char.level[level-1]={} end
   self.db.char.level[level].time = time()
+  self.db.char.level[level-1].playtime = self.playedthislevel + self.difftime
+  RequestTimePlayed()
 end
 
 function addon:MinTimer()
@@ -70,6 +103,6 @@ function addon:OnInitialize()
   self.db = LibStub("AceDB-3.0"):New("LeveltimeDB")
   self:RegisterEvent("TIME_PLAYED_MSG")
   self:RegisterEvent("PLAYER_LEVEL_UP")
-  self.minuteTimer = self:ScheduleRepeatingTimer("MinTimer", 60)
+  self.minuteTimer = self:ScheduleRepeatingTimer("MinTimer", 1)
   RequestTimePlayed()
 end
